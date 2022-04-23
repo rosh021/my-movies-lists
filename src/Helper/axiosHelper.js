01f15e95e29a9c6ef270dcbff621b31ab3217785
rosh021 <roshanbhattarai021@gmail.com>
2022-04-24 09:57:50 +1000

    bug fixed

--- src/Helper/axiosHelper.js
+++ src/Helper/axiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = "http://www.omdbapi.com/?apikey=c388f54&";
+const apiUrl = "https://www.omdbapi.com/?apikey=c388f54&";
 
 export const fetchMovie = (title) => {
   return axios.get(apiUrl + "t=" + title);
